d4f741d45bba23ac70df7d89b84398e6f4f968d1
Sergey Barkhatov <sergeybarkhat@yandex.ru>
2023-07-07 13:47:19 +0200

    Push

--- scripts/index.js
+++ scripts/index.js
@@ -27,3 +27,10 @@ $(document).ready(function(){
   });
 });
 
+var phoneMask = IMask(
+  document.getElementById('phone-mask'), {
+    mask: '+{7}(000)000-00-00'
+  });
+
+  console.log(phoneMask);
+
